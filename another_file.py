f4404a447d034d3ed17d09d715f9c5d0b6fd4d37
mcgregor94086 <scott@sonautics.com>
2022-07-23 19:09:12 -0700

    a change to another_file.py

--- another_file.py
+++ another_file.py
@@ -5,13 +5,13 @@ __author__ = ''
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_another(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_another('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
